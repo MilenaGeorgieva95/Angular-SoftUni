7a19f74843f6e2abaaed003cde5b589136ce1c2e
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-06-10 11:19:21 +0300

    show todo status

--- 04.DI, Intro to RxJS, Services/todo-list/src/app/todo-item/todo-item.component.ts
+++ 04.DI, Intro to RxJS, Services/todo-list/src/app/todo-item/todo-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { Todo } from '../types/todo';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent {
-@Input('todoTask') task:string=''
+  @Input('todoTask') todo: Todo = { task: '', completed: false };
 }
