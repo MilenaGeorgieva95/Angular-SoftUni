9b5c5668da8146c73d6be2a8fd2f0c69534c5ecb
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-06-19 13:57:03 +0300

    create main component wrapper, posts and themes components

--- 05.Components-Workshop/forum-app-workshop/src/app/app.module.ts
+++ 05.Components-Workshop/forum-app-workshop/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
