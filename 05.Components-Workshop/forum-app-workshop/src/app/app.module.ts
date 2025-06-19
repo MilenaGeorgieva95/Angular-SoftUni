cd3732476559597d7741423a9400906574452f94
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-06-19 13:44:47 +0300

    create CoreModule with header and Footer components and connect them

--- 05.Components-Workshop/forum-app-workshop/src/app/app.module.ts
+++ 05.Components-Workshop/forum-app-workshop/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
