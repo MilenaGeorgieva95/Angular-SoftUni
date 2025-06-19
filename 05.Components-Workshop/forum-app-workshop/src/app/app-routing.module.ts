033c07d3ba9e94372f35ad9292f7dda6f90e8bcf
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-06-19 14:07:16 +0300

    set up routing

---
+++ 05.Components-Workshop/forum-app-workshop/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- 05.Components-Workshop/forum-app-workshop/src/app/app.module.ts
+++ 05.Components-Workshop/forum-app-workshop/src/app/app.module.ts
@@ -7,16 +7,22 @@ import { SharedModule } from './shared/shared.module';
 import { MainComponent } from './main/main.component';
 import { ThemesListComponent } from './themes-list/themes-list.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     ThemesListComponent,
-    PostsListComponent
+    PostsListComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     CoreModule,
     SharedModule
   ],
