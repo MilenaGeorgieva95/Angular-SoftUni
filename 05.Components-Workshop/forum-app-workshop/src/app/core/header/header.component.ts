caba8d19400017f3691a7c5313379e1fccc3c038
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-06-23 08:47:31 +0300

    switch login/logout button on click

--- 05.Components-Workshop/forum-app-workshop/src/app/core/header/header.component.ts
+++ 05.Components-Workshop/forum-app-workshop/src/app/core/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  isLoggedIn = false;
+  loginUser():void {
+    this.isLoggedIn = true;
+  }
+  logoutUser():void {
+    this.isLoggedIn = false;
+  }
 }
