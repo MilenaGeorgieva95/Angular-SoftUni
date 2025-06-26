52bdd55764e00a8e19be396de04ef483517f1131
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-06-26 14:47:09 +0300

    add loader to themes list

--- 05.Components-Workshop/forum-app-workshop/src/app/posts-list/post-item/post-item.component.ts
+++ 05.Components-Workshop/forum-app-workshop/src/app/posts-list/post-item/post-item.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from 'src/app/types/post';
 
 @Component({
   selector: 'app-post-item',
   templateUrl: './post-item.component.html',
-  styleUrls: ['./post-item.component.css']
+  styleUrls: ['./post-item.component.css'],
 })
 export class PostItemComponent {
-
+  @Input('post') post = {} as Post;
 }
--- 05.Components-Workshop/forum-app-workshop/src/app/themes-list/themes-list.component.ts
+++ 05.Components-Workshop/forum-app-workshop/src/app/themes-list/themes-list.component.ts
@@ -5,12 +5,16 @@ import { Theme } from '../types/theme';
 @Component({
   selector: 'app-themes-list',
   templateUrl: './themes-list.component.html',
-  styleUrls: ['./themes-list.component.css']
+  styleUrls: ['./themes-list.component.css'],
 })
-export class ThemesListComponent implements OnInit{
-themes: Theme[] =[]
-constructor(private api:ApiService){}
-ngOnInit(): void {
-  this.api.getThemes().subscribe(themes=>this.themes=themes)
-}
+export class ThemesListComponent implements OnInit {
+  themes: Theme[] = [];
+  isLoading: boolean = true;
+  constructor(private api: ApiService) {}
+  ngOnInit(): void {
+    this.api.getThemes().subscribe((themes) => {
+      this.themes = themes;
+      this.isLoading = false;
+    });
+  }
 }
