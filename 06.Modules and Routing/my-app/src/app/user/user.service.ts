a4a4efccea5c1a4c878a3ed346eb0a856f46845f
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-02 14:11:50 +0300

    create environments

---
+++ 06.Modules and Routing/my-app/src/app/user/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  API = environment.UserApiUrl;
+  constructor() {}
+}
---
+++ 06.Modules and Routing/my-app/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    UserApiUrl :'https://jsonplaceholder.typicode.com/users'
+};
