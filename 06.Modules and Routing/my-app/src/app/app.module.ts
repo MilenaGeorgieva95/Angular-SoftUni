8aa72a4404c8dc0e08da0371286ba6ca277ba086
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-02 13:06:47 +0300

    create global loader

--- 06.Modules and Routing/my-app/src/app/app.module.ts
+++ 06.Modules and Routing/my-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     NoopAnimationsModule,
     SharedModule,
-  ],
+    CoreModule
+],
   providers: [],
   bootstrap: [AppComponent],
 })
