b8dc81bfdb2f4e81dddb56b3d1e15ef92f6b08bd
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-02 12:58:23 +0300

    create spinner component

--- 06.Modules and Routing/my-app/src/app/app.module.ts
+++ 06.Modules and Routing/my-app/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
