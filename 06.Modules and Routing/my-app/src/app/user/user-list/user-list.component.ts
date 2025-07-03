02ac1a894524b6c42c56fe234e262e33f91eb40a
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-03 13:39:50 +0300

    show loader while fetching users data

--- 06.Modules and Routing/my-app/src/app/user/user-list/user-list.component.ts
+++ 06.Modules and Routing/my-app/src/app/user/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { User } from 'src/app/types/user';
 
 @Component({
   selector: 'app-user-list',
@@ -8,6 +9,7 @@ import { UserService } from '../user.service';
 })
 export class UserListComponent implements OnInit {
   isLoading: boolean = false;
+  users: User[] = [];
 
   constructor(private userService: UserService) {}
 
@@ -16,6 +18,10 @@ export class UserListComponent implements OnInit {
   }
 
   fetchUsers() {
-    this.userService.getUsers().subscribe((users) => console.log(users));
+    this.isLoading = true;
+    this.userService.getUsers().subscribe((users) => {
+      this.users = users;
+      this.isLoading = false;
+    });
   }
 }
