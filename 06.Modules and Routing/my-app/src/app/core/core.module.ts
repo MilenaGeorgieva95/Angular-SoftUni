5c3ba21ac7270999e97ab3a2bc0326c2f998e4b6
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-03 20:10:14 +0300

    create nav component

--- 06.Modules and Routing/my-app/src/app/core/core.module.ts
+++ 06.Modules and Routing/my-app/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GlobalLoaderComponent } from './global-loader/global-loader.component';
 import { SharedModule } from '../shared/shared.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [GlobalLoaderComponent],
+  declarations: [GlobalLoaderComponent, NavComponent],
   imports: [CommonModule, SharedModule],
-  exports: [GlobalLoaderComponent],
+  exports: [GlobalLoaderComponent, NavComponent],
 })
 export class CoreModule {}
