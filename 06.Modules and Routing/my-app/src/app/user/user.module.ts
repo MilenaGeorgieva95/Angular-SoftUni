7d815018488a23c6b3c91afc8cbf0a88b19984ad
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-04 14:35:02 +0300

    create user details component

--- 06.Modules and Routing/my-app/src/app/user/user.module.ts
+++ 06.Modules and Routing/my-app/src/app/user/user.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserListComponent } from './user-list/user-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [UserListComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'user/list', component: UserListComponent },
+      { path: 'user/details', component: UserDetailsComponent },
+    ]),
+  ],
   exports: [UserListComponent],
 })
 export class UserModule {}
