e623a2773581b82a3dee602794762dfd380ff103
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-24 13:31:56 +0300

    set element style with Renderer2

--- 07.Directives-and-Forms/src/app/highlight.directive.ts
+++ 07.Directives-and-Forms/src/app/highlight.directive.ts
@@ -1,13 +1,15 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
-export class HighlightDirective implements OnInit{
-  constructor(private elRef: ElementRef) {}
+export class HighlightDirective implements OnInit {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    console.log(this.elRef.nativeElement);
-    this.elRef.nativeElement.style.background='orange'
+    //? this.elRef.nativeElement
+    //? this.elRef.nativeElement.style.background='orange'
+
+    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'purple');
   }
 }
