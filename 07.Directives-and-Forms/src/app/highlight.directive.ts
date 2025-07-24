ecf5a7afed07223c50f349d72b5b02a82f2a4626
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-07-24 13:57:50 +0300

    add @HostListener decorator to callback function

--- 07.Directives-and-Forms/src/app/highlight.directive.ts
+++ 07.Directives-and-Forms/src/app/highlight.directive.ts
@@ -1,9 +1,14 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective implements OnInit {
+@HostListener('mouseover', ['$event']) mouseOverhandler(e:MouseEvent){
+  console.log('mouseOver ');
+  
+}
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
