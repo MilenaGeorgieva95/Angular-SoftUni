95685322532f37de8f4f46a2609134f272eac14a
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-08-13 17:57:01 +0300

    play with interceptor functionality

--- 08.Pipes, Interceptors and Subjects/my-app/src/app/http-interceptor.interceptor.ts
+++ 08.Pipes, Interceptors and Subjects/my-app/src/app/http-interceptor.interceptor.ts
@@ -5,13 +5,20 @@ import {
   HttpEvent,
   HttpInterceptor,
   HTTP_INTERCEPTORS,
+  HttpResponse,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { catchError, EMPTY, Observable, tap } from 'rxjs';
+import { catchError, EMPTY, Observable, tap, throwError } from 'rxjs';
 import { API_URL } from './constants';
 
 @Injectable()
 export class HttpInterceptorInterceptor implements HttpInterceptor {
   constructor() {}
+
+  authService = { token: '123' };
+  saveToken(event: HttpEvent<any>) {
+    sessionStorage.setItem('UserToken', 'event');
+  }
 
   intercept(
     request: HttpRequest<unknown>,
@@ -19,21 +26,28 @@ export class HttpInterceptorInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<unknown>> {
     if (request.url.startsWith('/api')) {
       request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.authService.token}`,
+          'Content-Type': 'application/json',
+        },
         url: request.url.replace('/api', API_URL),
       });
     }
 
     return next.handle(request).pipe(
-      tap((req) => {
-        if (req instanceof HttpRequest) {
-          console.log(req);
+      tap((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse && request.url.endsWith('login')) {
+          this.saveToken(event);
+          console.log(event);
         }
       }),
       catchError((error) => {
-        if (error.status === 0) {
+        if (error.status === 401) {
           console.log('Error from Interceptor: ', error);
+          // this.router.navigate(['/login'])
           return EMPTY;
         }
+        // return throwError(error)
         return [error];
       })
     );
